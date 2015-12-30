67e73b1238ef15930c093273d7e71a5aa29d457d
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2015-12-30 12:00:44 +0200

    jsystem: Custom shell command executor

---
+++ wfdb-wrappers/jsystem.m
@@ -0,0 +1,22 @@
+function [ res, out ] = jsystem( cmd )
+%JSYSTEM Execute a shell command
+%   Executes a shell command as a subprocess using java's ProcessBuilder
+%   class. This is much faster than using the builtin matlab 'system'
+%   command.
+
+pb = java.lang.ProcessBuilder({'/bin/sh', '-c', cmd});
+pb.environment.put('PATH', [char(pb.environment.get('PATH')) ':/usr/local/bin']);
+pb.directory(java.io.File(pwd));
+pb.redirectErrorStream(true);
+
+process = pb.start();
+res = process.waitFor();
+
+is = process.getInputStream();
+scanner = java.util.Scanner(is).useDelimiter('\\A');
+if scanner.hasNext()
+    out = scanner.next();
+else
+    out = '';
+end
+end
