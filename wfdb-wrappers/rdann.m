03a5dad74ee9b09dcd55ffd22bf701e7b35333ca
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2016-01-25 06:01:02 +0200

    rdann: Bugfix for long signals
    In very long signals, the absolute time format that was previously used
    would add a column to the hours in some cases.
    
    Now we use the '-e' flag to specify that elapsed times should be
    printed, preventing this issue.

--- wfdb-wrappers/rdann.m
+++ wfdb-wrappers/rdann.m
@@ -17,7 +17,7 @@ p.parse(rec_name, ann_ext, varargin{:});
 %% === Run rdann
 
 % Command to run rdann and cut only the annotation samples out
-command = sprintf('rdann -r %s -a %s | awk ''{print $2}''', rec_name, ann_ext);
+command = sprintf('rdann -e -r %s -a %s | awk ''{print $2}''', rec_name, ann_ext);
 
 [res, out] = jsystem(command);
 if(res ~= 0)
@@ -25,7 +25,10 @@ if(res ~= 0)
 end
 
 % Convert string of numbers to vector
-ann = str2num(out); 
+[ann, conversion_ok] = str2num(out);
+if (conversion_ok == 0)
+    error('Failed to convert rdann output to samples');
+end
 
 % add 1 to all values because WFDB's indices are zero-based
 ann = ann + 1;
