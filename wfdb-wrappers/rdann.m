1b8b62f286d677ca67dcae491894a36c660609c1
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2016-02-11 01:32:35 +0200

    rdann: Support specifying annotation types
    The rdann wrapper now supports the '-p' option, which allows specifying
    a specific annotation type to read.

--- wfdb-wrappers/rdann.m
+++ wfdb-wrappers/rdann.m
@@ -5,19 +5,30 @@ function [ ann ] = rdann( rec_name, ann_ext, varargin )
 %% === Input
 
 % Defaults
+DEFAULT_ANN_TYPES = '';
 
 % Define input
 p = inputParser;
 p.addRequired('rec_name', @isrecord);
 p.addRequired('ann_ext', @isstr);
+p.addParameter('ann_types', DEFAULT_ANN_TYPES, @ischar);
 
 % Get input
 p.parse(rec_name, ann_ext, varargin{:});
+ann_types = p.Results.ann_types;
 
 %% === Run rdann
 
 % Command to run rdann and cut only the annotation samples out
-command = sprintf('rdann -e -r %s -a %s | awk ''{print $2}''', rec_name, ann_ext);
+command = sprintf('rdann -e -r %s -a %s', rec_name, ann_ext);
+
+% Add annotation types flag if necessary
+if (~isempty(ann_types))
+    command = sprintf('%s -p %s', command, ann_types);
+end
+
+% Pipe to awk to cut the second column of the output (sample numbers)
+ command =[command, ' | awk ''{print $2}'''];
 
 [res, out] = jsystem(command);
 if(res ~= 0)
@@ -25,12 +36,17 @@ if(res ~= 0)
 end
 
 % Convert string of numbers to vector
-[ann, conversion_ok] = str2num(out);
-if (conversion_ok == 0)
-    error('Failed to convert rdann output to samples');
+if (~isempty(out))
+    [ann, conversion_ok] = str2num(out);
+    if (conversion_ok == 0)
+        error('Failed to convert rdann output to samples');
+    end
+else
+    ann = [];
 end
 
 % add 1 to all values because WFDB's indices are zero-based
 ann = ann + 1;
+
 end
 
