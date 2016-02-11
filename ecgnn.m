89ca6e4f28b69c6f81af5875658ee7185110aed1
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2016-02-11 02:54:19 +0200

    ecgnn: Find NN-intervals in ECG data
    Use rqrs' outlier detection to remove RR intervals that might not be
    normal beats, thus obtaining the NN intervals.

---
+++ ecgnn.m
@@ -0,0 +1,50 @@
+function [ nni, tnn, rri, trr ] = ecgnn(rec_name, varargin)
+%ECGNN Calculate an NN-interval time series from ECG signal.
+%   rec_name - ECG signal record (physionet format)
+%   nni      - NN-intervals
+%   tnn      - NN-interval times
+%   rri      - RR-intervals
+%   trr      - RR-interval times
+
+%% === Input
+
+% Define input
+p = inputParser;
+p.KeepUnmatched = true;
+p.addRequired('rec_name', @isrecord);
+
+%% Find R peaks
+[ qrs, tm, ~, ~, qrs_outliers ] = rqrs(rec_name, 'gqpost', true, varargin{:});
+
+%% Find RR intervals
+
+% RR-intervals are the time-difference between R-Peaks
+rri = diff(tm(qrs));
+trr = tm(qrs(1:end-1));
+
+%% Find NN intervals
+% In case a qrs detection is a suspected outlier we assume it's not a normal (N) beat,
+% so we don't want to take the interval before or after it.
+
+% create a time axis where outliers are marked with NaN
+tm_nan = tm;
+tm_nan(qrs_outliers) = NaN;
+
+% Calculate time difference of qrs detections. The outliers were marked
+% with NaN so diff will cause two intervals to be also marked with NaN (which is what we
+% want since for every outlier we need to drop two intervals).
+nni = diff(tm_nan(qrs));
+
+% Discard NaN intervals
+nni(isnan(nni)) = [];
+
+% For each NaN index in the time axis, also mark the previous index as NaN
+% (since we drop two intervals per outlier)
+tnn = tm_nan(qrs(1:end-1));
+nanidx = find(isnan(tnn));
+nanidx = [nanidx; nanidx-1];
+
+% Take RR interval times and remove the marked indexes
+tnn(nanidx) = [];
+
+end
