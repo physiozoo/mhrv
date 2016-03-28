e4eb2650050769e19ea781a81ed3c8b8fe4267ff
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2016-03-28 06:44:42 +0300

    filternn: Remove the BPM filter
    This filter is redundant when also using the window filter.

--- filternn.m
+++ filternn.m
@@ -1,13 +1,11 @@
 function [ tnn_filtered, nni_filtered ] = filternn( tnn, nni, varargin )
 %FILTERNN Filters NN intervals to remove possible outliers
-%   Performs three types of filtering of NN intervals
-%   1)  Removes intervals that are non-physiological (single interval is too
-%       short or too long).
-%   2)  Averaging filter: If an interval is greater (abs) than X percent of
-%       the average in a window around it, excludes the interval. The window
-%       average is calculated without the sampe itself.
+%   Performs filtering of NN intervals using an averaging filter.
+%   If an interval is greater (abs) than X percent of the average in a
+%   window around it, excludes the interval. The window average is
+%   calculated without the sample itself.
 %
-%   These filters and their defult values are based on:
+%   Based on:
 %   1) PhysioNet HRV toolkit: https://physionet.org/tutorials/hrv-toolkit/
 %   2) Moody, G. B. (1993). Spectral analysis of heart rate without resampling.
 %      Computers in Cardiology 1993, Proceedings., (1), 7?10.
@@ -16,8 +14,6 @@ function [ tnn_filtered, nni_filtered ] = filternn( tnn, nni, varargin )
 %% === Input
 
 % Defaults
-DEFAULT_MIN_NN = 0.3; % seconds
-DEFAULT_MAX_NN = 2.0; % seconds
 DEFAULT_WIN_SAMPLES = 20; % samples
 DEFAULT_WIN_PERCENT = 20; % percentage [0-100]
 
@@ -26,16 +22,12 @@ p = inputParser;
 p.KeepUnmatched = true;
 p.addRequired('tnn', @(x) isnumeric(x) && ~isscalar(x));
 p.addRequired('nni', @(x) isnumeric(x) && ~isscalar(x));
-p.addParameter('min_nn', DEFAULT_MIN_NN, @isnumeric);
-p.addParameter('max_nn', DEFAULT_MAX_NN, @isnumeric);
 p.addParameter('win_samples', DEFAULT_WIN_SAMPLES, @isnumeric);
 p.addParameter('win_percent', DEFAULT_WIN_PERCENT, @(x) x >= 0 && x <= 100);
 p.addParameter('plot', nargout == 0, @islogical);
 
 % Get input
 p.parse(tnn, nni, varargin{:});
-min_nn = p.Results.min_nn;
-max_nn = p.Results.max_nn;
 win_samples = p.Results.win_samples;
 win_percent = p.Results.win_percent;
 should_plot = p.Results.plot;
@@ -49,10 +41,6 @@ end
 nni_filtered = nni;
 tnn_filtered = tnn;
 
-%% Filter single intervals
-
-outlier_idx1 = find( nni < min_nn | nni > max_nn );
-
 %% Filter interval in window
 
 % Filter the NN intervals with a moving average window
@@ -60,39 +48,34 @@ b_fir = 1/(2 * win_samples) .* [ones(win_samples,1); 0; ones(win_samples,1)];
 nni_lp = filtfilt(b_fir, 1, nni); % using filtfilt for zero-phase
 
 % Find and remove outliers
-outlier_idx2 = find( abs(nni - nni_lp) > (win_percent/100) .* nni_lp );
-
-%% Results
-
-all_outliers_idx = [outlier_idx1; outlier_idx2];
-nni_filtered(all_outliers_idx) = [];
-tnn_filtered(all_outliers_idx) = [];
+outlier_idx = find( abs(nni - nni_lp) > (win_percent/100) .* nni_lp );
+nni_filtered(outlier_idx) = [];
+tnn_filtered(outlier_idx) = [];
 
 %% Plot if no output args
 if (should_plot)
+    fontsize = 14;
+    markersize = 10.0;
+    set(0,'DefaultAxesFontSize',fontsize);
     figure; hold on; grid on;
-    xlabel('time [s]'); ylabel('Intervals [s]');
+    xlabel('time [s]'); ylabel('RR Interval length [s]');
 
-    % Plot original vs. filtered
-    plot(tnn, nni); % original intervals
-    plot(tnn_filtered, nni_filtered, 'g:'); % filtered intervals
-    legend_labels = {'original', 'filtered'};
-    
-    % Plot outliers with non-physiological interval lengths
-    if (~isempty(outlier_idx1))
-        plot(tnn(outlier_idx1), nni(outlier_idx1), 'kx'); % outliers as x's
-        legend_labels{end+1} = 'outliers1';
-    end
-    
+    % Plot original intervals
+    plot(tnn, nni);
+    legend_labels = {'RR intervals'};
+
     % Plot outliers found by averaging
-    if (~isempty(outlier_idx2))
-        plot(tnn(outlier_idx2), nni(outlier_idx2), 'ro'); % outliers as circles
-        legend_labels{end+1} = 'outliers2';
+    if (~isempty(outlier_idx))
+        plot(tnn(outlier_idx), nni(outlier_idx), 'ko', 'MarkerSize', markersize);
+        legend_labels{end+1} = 'filtered intervals';
     end
 
-    % plot averages
-    plot(tnn, nni_lp, 'k', tnn, nni_lp.*(1.0-win_percent/100), 'k--', tnn, nni_lp.*(1.0+win_percent/100), 'k--');
-    legend_labels = [legend_labels, {'average', 'thresholds'}];
+    % Plot window average and thresholds
+    lower_threshold = nni_lp.*(1.0-win_percent/100);
+    upper_threshold = nni_lp.*(1.0+win_percent/100);
+    plot(tnn, nni_lp, 'k', tnn, lower_threshold, 'k--', tnn, upper_threshold, 'k--');
+    legend_labels = [legend_labels, {'window average', 'window threshold'}];
+    
     legend(legend_labels);
 end
 
