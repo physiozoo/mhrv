33d00476d7724c5aeefaf5342f8c7dcc75c90c54
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2016-03-29 06:11:55 +0300

    code reorganization
    Move the HRV toolset into src/, and it's dependencies (currently only
    the wfdb wrappers) into lib/.
    
    Created a bootstrap script to initialize the matlab environment for this
    dir structure.

---
+++ bootstrap_env.m
@@ -0,0 +1,24 @@
+%% BOOTSTRAP_ENV
+% Initialize path and environment for the rhrv tools.
+
+%% Reset workspaces
+close all;
+clear variables;
+
+%% Set up path
+
+% Find source and dependencies directories
+basepath_ = fileparts(mfilename('fullpath'));
+lib_dir_ = [basepath_ '/lib/'];
+src_dir_ = [basepath_ '/src/'];
+
+% Add them to matlab's path including subfolders
+addpath(genpath(lib_dir_));
+addpath(genpath(src_dir_));
+
+% Save database folder as a global variable
+global db_dir
+db_dir = [basepath_ '/db/'];
+
+%% Clean up
+clear -regexp _$
