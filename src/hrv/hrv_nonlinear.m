7969cc08df11542dcbb2c37ba5d2cb52a7a1e14b
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2016-05-03 06:10:03 +0300

    hrv_nonlinear: Calculate MSE
    Added multiscale entropy calculation. The values are currently only
    plotted, not used. Need to decide on the metric to implement based on
    the MSE values.

--- src/hrv/hrv_nonlinear.m
+++ src/hrv/hrv_nonlinear.m
@@ -8,6 +8,9 @@ DEFAULT_ALPHA2_RANGE = [16, 128];
 DEFAULT_NMIN = 3;
 DEFAULT_NMAX = 150;
 DEFAULT_BETA_BAND = [0.003, 0.04]; % hz
+DEFAULT_MSE_MAX_SCALE = 20;
+DEFAULT_SAMPEN_R = 0.15; % percent of std. dev.
+DEFAULT_SAMPEN_M = 2;
 
 % Define input
 p = inputParser;
@@ -19,6 +22,9 @@ p.addParameter('alpha2_range',  DEFAULT_ALPHA2_RANGE, @(x) isnumeric(x) && numel
 p.addParameter('n_min',  DEFAULT_NMIN, @(x) isnumeric(x) && isscalar(x));
 p.addParameter('n_max',  DEFAULT_NMAX, @(x) isnumeric(x) && isscalar(x));
 p.addParameter('beta_band',  DEFAULT_BETA_BAND, @(x) isnumeric(x) && numel(x) == 2);
+p.addParameter('mse_max_scale', DEFAULT_MSE_MAX_SCALE, @(x) isnumeric(x) && isscalar(x));
+p.addParameter('sampen_r', DEFAULT_SAMPEN_R, @(x) isnumeric(x) && isscalar(x));
+p.addParameter('sampen_m', DEFAULT_SAMPEN_M, @(x) isnumeric(x) && isscalar(x));
 p.addParameter('plot', nargout == 0, @islogical);
 
 % Get input
@@ -28,6 +34,9 @@ alpha2_range = p.Results.alpha2_range;
 n_min = p.Results.n_min;
 n_max = p.Results.n_max;
 beta_band = p.Results.beta_band;
+mse_max_scale = p.Results.mse_max_scale;
+sampen_r = p.Results.sampen_r;
+sampen_m = p.Results.sampen_m;
 should_plot = p.Results.plot;
 
 %% === DFA
@@ -93,6 +102,12 @@ f_axis_log = log10(f_axis(beta_band_idx));
 pxx_fit_beta = polyfit(f_axis_log, pxx_log, 1);
 hrv_nl.beta = pxx_fit_beta(1);
 
+%% === Nonlinear metrics (Multiscale sample entropy)
+
+[ mse_values, scale_axis ] = mse(nni, 'mse_max_scale', mse_max_scale, 'sampen_m', sampen_m, 'sampen_r',sampen_r);
+
+% TODO: Decide what metric to calculate from the mse values.
+
 %% === Display output if requested
 if (should_plot)
     set(0,'DefaultAxesFontSize',14);
@@ -100,7 +115,7 @@ if (should_plot)
     figure;
 
     % Plot the DFA data
-    subplot(2, 1, 1);
+    subplot(3, 1, 1);
     loglog(DFA_n, DFA_Fn, 'ko', 'MarkerSize', 7);
     hold on; grid on;
 
@@ -120,7 +135,7 @@ if (should_plot)
     f_beta_plot = f_axis(beta_band_idx);
     pxx_beta_plot = pxx(beta_band_idx);
     decimation_factor = 10;
-    subplot(2, 1, 2);
+    subplot(3, 1, 2);
     loglog(f_beta_plot(1:decimation_factor:end), pxx_beta_plot(1:decimation_factor:end), 'ko', 'MarkerSize', 7);
     hold on; grid on;
 
@@ -129,4 +144,10 @@ if (should_plot)
     loglog(10.^f_axis_log, 10.^beta_line, 'Color', 'magenta', 'LineStyle', ls, 'LineWidth', lw);
     xlabel('log(frequency [hz])'); ylabel('log(PSD [s^2/Hz])');
     legend('PSD', ['\beta = ' num2str(hrv_nl.beta)]);
+
+    % Plot MSE
+    subplot(3, 1, 3);
+    plot(scale_axis, mse_values, '--ko', 'MarkerSize', 7); grid on;
+    xlabel('Scale factor'); ylabel('Sample Entropy');
+    legend(['MSE, ', 'r=' num2str(sampen_r), ' m=' num2str(sampen_m)]);
 end
