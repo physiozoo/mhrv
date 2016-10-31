8a617a295d698f18694b19224ed90710b70efc12
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2016-10-31 14:23:10 +0200

    Added config file
    Created a config file, cfg/rhrv_config.m, that a user can edit to set
    preferences like machine specific paths before running the bootstrap
    script.
    
    Also renamed bootstrap_env.m -> bootstrap.m.

---
+++ bootstrap.m
@@ -0,0 +1,33 @@
+%% BOOTSTRAP
+% Please run this script before using the toolkit.
+% It will initialize the matlab path and environment for the rhrv tools.
+% To set custom user-specific options, edit the file cfg/rhrv_config.m before running this.
+
+%% Reset workspaces
+close all;
+clear variables;
+
+%% Set up matlab path
+
+% Find source and dependencies directories
+basepath_ = fileparts(mfilename('fullpath'));
+lib_dir_ = [basepath_ '/lib/'];
+src_dir_ = [basepath_ '/src/'];
+cfg_dir_ = [basepath_ '/cfg/'];
+
+% Add them to matlab's path including subfolders
+addpath(genpath(lib_dir_));
+addpath(genpath(src_dir_));
+
+%% Load user configuration
+run([cfg_dir_ '/rhrv_config.m']);
+
+% Add wfdb location to jsystem path
+global jsystem_path;
+jsystem_path = {rhrv_cfg_.paths.wfdb_path};
+
+% Default font size for figres
+set(0,'DefaultAxesFontSize', rhrv_cfg_.plots.font_size);
+
+%% Clean up
+clear -regexp _$
---
+++ cfg/rhrv_config.m
@@ -0,0 +1,14 @@
+%%%  Configuration file for the rhrv toolkit.
+%%%
+%%% Use this file to set user-specific settings.
+
+%% Configure paths
+% All paths here can be relaive (to the root of the repo), or absolute.
+
+% Specify a path on this system that contains the physionet wfdb executables.
+rhrv_cfg_.paths.wfdb_path = '/usr/local/bin';
+
+%% Configure plots
+
+% Specify desired font size for figures.
+rhrv_cfg_.plots.font_size = 14;
