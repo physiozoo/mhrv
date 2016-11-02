e1838e03e038179bbf81148a1d9a192314055f30
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2016-11-02 15:05:51 +0200

    read_acq: Moved up
    This is a general utility function for reading ACQ format files, so
    moved it up into the data_import folder.
    
    Added some documentation for using it.

--- src/data_import/read_acq.m
+++ src/data_import/read_acq.m
@@ -1,6 +1,16 @@
 function [t, data, fs, info] = read_acq(filename)
 %READ_ACQ Load/plot data files in *.acq format into matlab.
-%   Detailed explanation goes here
+%
+% Input:
+%   filename: Path to *.acq file to read. Can be relative to pwd.
+% Output:
+%   t: Cell array containing the time axis values for each channel.
+%   data: Cell array containing the data values for each channel.
+%   fs: The sampling frequency in Hz (for all channels).
+%   info: A struct containing additional information that was read form the file.
+%
+% If no output variables are provided, this function will plot the data (each channel in a subplot).
+%
 
 % Read raw data (use 'acqread' from matlab file exchange)
 [info,data] = acqread(filename);
@@ -14,30 +24,31 @@ t = cell(1, n_channels);
 
 for chan_idx = 1:n_channels
     n = length(data{chan_idx});
-    
+
     % Scale the data
     scale = double(info.dAmplScale(chan_idx));
     offset = double(info.dAmplOffset(chan_idx));
     data{chan_idx} = double(data{chan_idx}) .* scale + offset;
 
     % Create time axis (in seconds)
-    t{chan_idx} = (1/fs) .* (0:(n-1));
+    t{chan_idx} = (1/fs) .* (0:(n-1))';
 end
 
 if (nargout == 0)
     figure;
     n_markers = double(info.lMarkers);
     colors = cool(n_markers+1);
-    
+    [~, filename_without_path, ~] = fileparts(filename);
+
     for chan_idx = 1:n_channels
         subplot(n_channels, 1, chan_idx); grid on;
         plot(t{chan_idx}, data{chan_idx}, 'Color', 'blue');
         ylabel(info.szUnitsText(chan_idx));
         xlabel('Seconds');
-        
+
         legend_entries = cell(1, n_markers+1);
         legend_entries{1} = info.szCommentText{chan_idx};
-        
+
         % Add annotations
         for marker_idx = 1:n_markers
             line_x = ones(1,2) * double(info.lSample(marker_idx)) * 1/fs;
@@ -48,4 +59,7 @@ if (nargout == 0)
 
         legend(legend_entries);
     end
+
+    % Add title above all subplots
+    mtit(filename_without_path);
 end
