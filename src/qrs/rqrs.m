ade66da12baacfcf604ca04fcdbb990a87676aa4
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2017-01-04 12:36:22 +0200

    rqrs: Auto calculation of window size
    If a gqrs config file was provided, rqrs can now calculate the
    window_size_sec parameter automatically by parsing the 'QS' parameter
    (QRS duration) from the config file.

--- src/qrs/rqrs.m
+++ src/qrs/rqrs.m
@@ -5,26 +5,50 @@ function [ qrs, outliers ] = rqrs( rec_name, varargin )
 %% === Input
 
 % Defaults
-DEFAULT_WINDOW_SIZE_SECONDS = 0.1;
+DEFAULT_GQPOST = true;
+DEFAULT_WINDOW_SIZE_SECONDS = 0.056; % 80% of .07, the average human QRS duration
 
 % Define input
 p = inputParser;
 p.KeepUnmatched = true;
 p.addRequired('rec_name', @isrecord);
+p.addParameter('gqpost', DEFAULT_GQPOST, @(x) islogical(x) && isscalar(x));
+p.addParameter('gqconf', '', @isstr);
 p.addParameter('window_size_sec', DEFAULT_WINDOW_SIZE_SECONDS, @isnumeric);
 
 % Get input
 p.parse(rec_name, varargin{:});
+gqpost = p.Results.gqpost;
+gqconf = p.Results.gqconf;
 window_size_sec = p.Results.window_size_sec;
 
 %% === Run gqrs
 ecg_col = get_signal_channel(rec_name);
-[gqrs_detections, gqrs_outliers] = gqrs(rec_name, 'ecg_col', ecg_col, varargin{:});
+[gqrs_detections, gqrs_outliers] = gqrs(rec_name, 'ecg_col', ecg_col, 'gqpost', gqpost, 'gqconf', gqconf);
 
-% === Read Signal
+%% === Read Signal
 [tm, sig, ~] = rdsamp(rec_name, ecg_col);
 
-% === Augment gqrs detections
+%% === Try Set window size intelligently from config
+% Check whether the window size was specfied explicitly by caller. If so,
+% use that. Otherwise, if gqconf was specified, look for the QS parameter
+% in the file and set window size based on it.
+
+window_size_was_specified = ~any(cellfun(@(s) strcmp(s,'window_size_sec'), p.UsingDefaults));
+gqconf_was_specified      = ~any(cellfun(@(s) strcmp(s,'gqconf'),          p.UsingDefaults));
+
+if gqconf_was_specified && ~window_size_was_specified
+    % Parse QS from config file
+    qs = read_qs_from_conf(gqconf);
+
+    % If QS was defined in the config file, use 80% of it as window size, otherwise leave the
+    % default value for window size.
+    if ~isnan(qs)
+        window_size_sec = 0.8 * qs;
+    end
+end
+
+%% === Augment gqrs detections
 [ qrs, outliers ] = rqrs_augment(gqrs_detections, gqrs_outliers, tm, sig, 'window_size_sec', window_size_sec);
 
 % Plot if no output arguments
@@ -39,6 +63,18 @@ if (nargout == 0)
         legend('signal', 'qrs detections', 'suspected outliers');
     else
         legend('signal', 'qrs detections');
+    end
+end
+
+%% === Helper functions
+
+% Parses the gqconf file to extract the 'QS' parameter
+function qs = read_qs_from_conf(gqconf)
+    qs = NaN;
+    config_text = fileread(gqconf);
+    tokens = regexpi(config_text, '(?:[\n]|^)\s*QS\s+(\d*\.?\d+)', 'tokens');
+    if ~isempty(tokens)
+        qs = str2double(tokens{end});
     end
 end
 
