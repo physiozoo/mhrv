0d40750edbce6d3b1ac36ff457bbd1f378e83590
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2017-02-15 10:34:38 +0200

    get_wfdb_tool_path: Support custom search path
    Allow the get_wfdb_tool_path function to search a custom path that
    defaults to pwd.

--- lib/wfdb-wrappers/get_wfdb_tool_path.m
+++ lib/wfdb-wrappers/get_wfdb_tool_path.m
@@ -1,12 +1,14 @@
-function [ tool_path ] = get_wfdb_tool_path( tool_name )
+function [ tool_path ] = get_wfdb_tool_path( tool_name, base_search_path )
 %GET_WFDB_TOOL_PATH Returns the path to a wfdb tool, takes OS into account
-%   Looks for the given tool recursively under the current MATLAB directory, and then recursively
-%   under the folders in the $PATH environment variable. In case the tool is found,
+%   Looks for the given tool recursively under the current MATLAB directory (or a given directory),
+%   and then recursively under the folders in the $PATH environment variable. In case the tool is found,
 %   the containing directory path will be persisted to speed up the next search.
 %
 %   Inputs:
 %       tool_name - A string containg the name of the wfdb tool, e.g. 'gqrs',
 %       'wfdb-config', 'rdsamp' etc. Should not include a file extension.
+%       base_search_path - Optional. An absolute path to use as a base for searching for the tool.
+%                          If not provided defaults to pwd().
 %   Output:
 %       tool_path - The path of the wfdb tool, including it's os-specific
 %       file extension (e.g. .exe). In case the tool wasn't found, an error
@@ -15,6 +17,11 @@ function [ tool_path ] = get_wfdb_tool_path( tool_name )
 % Make sure tool name format is valid
 if (~ischar(tool_name) || isempty(tool_name))
     error(['invalid wfdb tool name ''', tool_name, '''']);
+end
+
+% Default base search path if it wasn't provided
+if (nargin == 1)
+    base_search_path = pwd;
 end
 
 % On windows, append .exe to the tool name
@@ -35,8 +42,8 @@ if ~isempty(wfdb_path)
     end
 end
 
-% Search under pwd and $PATH for the tool.
-search_path = [strsplit(genpath('.'), pathsep()), strsplit(getenv('PATH'), pathsep())];
+% Search under base_search_path and $PATH for the tool.
+search_path = [strsplit(genpath(base_search_path), pathsep()), strsplit(getenv('PATH'), pathsep())];
 
 % Remove anything under .git/ and remove empty entries
 filter_ind = cellfun(@(x) ~isempty(x) && isempty(regexp(x, '[.]git', 'once')), search_path);
