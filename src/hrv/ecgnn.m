5a5c22ab2f1b840ce4d3ed053ac278b2b44ede74
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2017-03-07 13:24:30 +0200

    ecgnn/filternn: rewrite & poincaré support
    Combined the RR-interval calculation and filtering into the same
    function. This allows much better visualization of what the different
    filters do.
    
    There are now three filters (outlier detectors for RR-intervals):
    PhysioNet's gqpost, poincaré-based ellipse fitting and time-based
    lowpass filtering.

--- src/hrv/ecgnn.m
+++ src/hrv/ecgnn.m
@@ -1,118 +1,193 @@
-function [ nni, tnn, rri, trr ] = ecgnn(rec_name, varargin)
-%ECGNN Calculate an NN-interval time series from ECG signal.
+function [ nni, tnn, rri, trr, rri_lp, tresh_low, thresh_high ] = ecgnn(rec_name, varargin)
+%ECGNN Calculate an NN-interval time series (filtered RR intervals) from ECG signal.
 % Detects QRS in a given sigal, performs outlier detection and removal and calculates the beat intervals.
+% This function can perform three types of different outlier detection, based on user input. By
+% default, it will perform all three.
 %   Inputs:
 %       - rec_name: Path and name of a wfdb record's files e.g. db/mitdb/100 if the record files (both
 %                   100.dat and 100.hea) are in a folder named 'db/mitdb' relative to MATLABs pwd.
 %       - varargin: Pass in name-value pairs to configure advanced options:
-%           - 'gqpost': true/false whether to use gqpost to post-process QRS detections and find
-%                       potentially wrong detections (suspected outliers).
-%           - 'gqconf': Path to a config file for gqrs. E.g. for analyzing non-human data it's
-%                       necessary to provide gqrs with an appropriate config file for the data.
-%           - 'use_rqrs': true/false whether to use the rqrs algorithm to detect R-peaks (gqrs
-%                         detects the onset of the QRS complex).
-%           - 'plot': true/false whether to generate a plot. Defaults to true if no output
-%                     arguments were specified.
+%           - gqconf: Path to a config file for gqrs. E.g. for analyzing non-human data it's
+%                     necessary to provide gqrs with an appropriate config file for the data.
+%           - use_rqrs: true/false whether to use the rqrs algorithm to detect R-peaks (gqrs
+%                       detects the onset of the QRS complex).
+%           - filter_gqpost: true/false whether to use gqpost to post-process QRS detections to
+%                            find potentially wrong qrs-detections.
+%           - filter_poincare: true/false whether to use poincare ellipse-fitting to detect
+%                              potential outliers in the rr-intervals.
+%           - filter_lowpass: true/false whether to use an averaging filter to detect potential
+%                             outliers in the rr-intervals. If an interval is greater (abs) than
+%                             'win_percent' percent of the average in a window of size
+%                             'win_samples' around it, excludes the interval.
+%           - win_samples: Number of samples in the filter window on each side of the current sample
+%                          (total window size will be 2*win_samples+1)
+%           - win_percent: The percentage above/below the average to use for filtering.
+%           - plot: true/false whether to generate a plot. Defaults to true if no output arguments
+%                   were specified.
 %   Outputs:
-%       - nni: NN-intervals values in seconds
+%       - nni: NN-intervals (RR intervals after removing outliers) values in seconds
 %       - tnn: NN-interval times in seconds
 %       - rri: RR-intervals values in seconds (Original intervals, before outlier removal)
 %       - trr: RR-interval times in seconds
+%       - rri_lp: Values of averaging filter (if 'filter_lowpass' was used)
+%       - tresh_low: Values of lower-filtering threshold (if 'filter_lowpass' was used)
+%       - tresh_high: Values of higher-filtering threshold (if 'filter_lowpass' was used)
 
-%% === Input
+%% Input
 
 % Defaults
-DEFAULT_GQPOST = true;
 DEFAULT_GQCONF = '';
 DEFAULT_USE_RQRS = true;
-DEFAULT_USE_POINCARE = false;
+DEFAULT_FILTER_GQPOST = true;
+DEFAULT_FILTER_POINCARE = true;
+DEFAULT_FILTER_LOWPASS = true;
+DEFAULT_WIN_SAMPLES = 20; % samples
+DEFAULT_WIN_PERCENT = 20; % percentage [0-100]
 
 % Define input
 p = inputParser;
 p.KeepUnmatched = true;
 p.addRequired('rec_name', @isrecord);
-p.addParameter('gqpost', DEFAULT_GQPOST, @(x) islogical(x) && isscalar(x));
 p.addParameter('gqconf', DEFAULT_GQCONF, @isstr);
 p.addParameter('use_rqrs', DEFAULT_USE_RQRS, @(x) islogical(x) && isscalar(x));
-p.addParameter('use_poincare', DEFAULT_USE_POINCARE, @(x) islogical(x) && isscalar(x));
+p.addParameter('filter_gqpost', DEFAULT_FILTER_GQPOST, @(x) islogical(x) && isscalar(x));
+p.addParameter('filter_poincare', DEFAULT_FILTER_POINCARE, @(x) islogical(x) && isscalar(x));
+p.addParameter('filter_lowpass', DEFAULT_FILTER_LOWPASS, @(x) islogical(x) && isscalar(x));
+p.addParameter('win_samples', DEFAULT_WIN_SAMPLES, @isnumeric);
+p.addParameter('win_percent', DEFAULT_WIN_PERCENT, @(x) x >= 0 && x <= 100);
 p.addParameter('plot', nargout == 0, @islogical);
 
 % Get input
 p.parse(rec_name, varargin{:});
-gqpost = p.Results.gqpost;
+filter_gqpost = p.Results.filter_gqpost;
 gqconf = p.Results.gqconf;
 use_rqrs = p.Results.use_rqrs;
-use_poincare = p.Results.use_poincare;
+filter_poincare = p.Results.filter_poincare;
+filter_lowpass = p.Results.filter_lowpass;
+win_samples = p.Results.win_samples;
+win_percent = p.Results.win_percent;
 should_plot = p.Results.plot;
 
-% === Read the signal
+% Check output
+if (nargout > 4 && ~filter_lowpass)
+    error('Low-pass filter outputs can''t be set without ''filter_lowpass'' option');
+end
+
+%% QRS detection
+
+% Read the signal
 ecg_col = get_signal_channel(rec_name);
 [tm, sig, ~] = rdsamp(rec_name, ecg_col);
 
-%% === Find QRS in the signal
-
 % Use gqrs to find QRS complex locations
-[qrs, qrs_outliers] = gqrs(rec_name, 'ecg_col', ecg_col, 'gqpost', gqpost, 'gqconf', gqconf);
+[qrs, qrs_outliers] = gqrs(rec_name, 'ecg_col', ecg_col, 'gqpost', filter_gqpost, 'gqconf', gqconf);
 
-% Use rqrs to find the r-peaks based on the qrs complex locations
+% Use rqrs to find the R-peaks based on the qrs complex locations
 if (use_rqrs)
     [qrs, qrs_outliers] = rqrs_augment(qrs, qrs_outliers, tm, sig);
 end
 
-%% Find RR intervals
+%% RR intervals
 
 % RR-intervals are the time-difference between R-Peaks
 rri = diff(tm(qrs));
 trr = tm(qrs(1:end-1));
 
-%% Find NN intervals
-% In case a qrs detection is a suspected outlier we assume it's not a normal (N) beat,
-% so we don't want to take the interval before or after it.
+%% GQPOST-based outlier detection
 
-% create a time axis where outliers are marked with NaN
-tm_nan = tm;
-tm_nan(qrs_outliers) = NaN;
+gqpost_outliers = [];
+if (filter_gqpost)
+    % create a time axis where outliers are marked with NaN
+    tm_nan = tm;
+    tm_nan(qrs_outliers) = NaN;
 
-% Calculate time difference of qrs detections. The outliers were marked
-% with NaN so diff will cause two intervals to be also marked with NaN (which is what we
-% want since for every outlier we need to drop two intervals).
-nni = diff(tm_nan(qrs));
+    % For each NaN index in the time axis, also mark the previous index as NaN
+    % (since we drop two intervals per outlier)
+    trr_nan = tm_nan(qrs(1:end-1));
+    nanidx = find(isnan(trr_nan));
 
-% Discard NaN intervals
-nni(isnan(nni)) = [];
-
-% For each NaN index in the time axis, also mark the previous index as NaN
-% (since we drop two intervals per outlier)
-tnn = tm_nan(qrs(1:end-1));
-nanidx = find(isnan(tnn));
-nanidx = [nanidx; nanidx-1];
-
-% Take RR interval times and remove the marked indexes
-tnn(nanidx) = [];
+    % Set the outlier indexes in of the RR intervals: For each GQPOST outlier, we have two intervals
+    % that are considered outliers (the interval before the outlier peak and the one after).
+    gqpost_outliers = [nanidx; nanidx-1];
+end
 
 %% Poincare-based outlier detection
 
-if (use_poincare)
-    [~, ~, poincare_outliers] = poincare(nni);
-    nni(poincare_outliers) = [];
-    tnn(poincare_outliers) = [];
+poincare_outliers = [];
+if (filter_poincare)
+    [~, ~, poincare_outliers] = poincare(rri, 'plot', should_plot);
 end
+
+%% Lowpass-filter-based outlier detection
+
+lp_outliers = [];
+if (filter_lowpass)
+    % Filter the NN intervals with a moving average window
+    b_fir = 1/(2 * win_samples) .* [ones(win_samples,1); 0; ones(win_samples,1)];
+    rri_lp = filtfilt(b_fir, 1, rri); % using filtfilt for zero-phase
+
+    % Find outliers
+    lp_outliers = find( abs(rri - rri_lp) > (win_percent/100) .* rri_lp );
+
+    % Save threshold lines if necessary
+    if (nargout > 4 || should_plot)
+        tresh_low   = rri_lp.*(1.0-win_percent/100);
+        thresh_high = rri_lp.*(1.0+win_percent/100);
+    end
+end
+
+%% Calculate filtered intervals
+
+all_outliers = unique([gqpost_outliers(:); poincare_outliers(:); lp_outliers(:)]);
+tnn = trr;
+nni = rri;
+
+tnn(all_outliers) = [];
+nni(all_outliers) = [];
 
 %% Plot if no output args or if requested
 if (should_plot)
-    figure; %hold on; grid on;
-    p1 = subplot(2,1,1);
-    plot(tm, sig); hold on; grid on;
-    set( get(p1,'XLabel'), 'String', 'time [s]' );
-    set( get(p1,'YLabel'), 'String', 'ECG [mV]' );
+    markersize = 8.0;
 
-    ihr = 60 ./ nni;
-    ihr_avg = mean(ihr);
-    p2 = subplot(2,1,2);
-    plot(tnn, ihr, tnn, ones(size(ihr)) .* ihr_avg); hold on; grid on;
-    set( get(p2,'XLabel'), 'String', 'time [s]' );
-    set( get(p2,'YLabel'), 'String', 'IHR [BPM]' );
-    legend('IHR', sprintf('Avg.=%f', ihr_avg));
+    figure; hold on; grid on;
+    plot(tm, sig);
+    plot(tm(qrs), sig(qrs,1), 'rx', 'MarkerSize', markersize);
+    xlabel('time [s]'); ylabel('ECG [mV]');
+    legend('ECG signal', sprintf('%s-peaks', char(use_rqrs * 'R' + (~use_rqrs)*'Q')));
+
+    figure; hold on; grid on;
+    xlabel('time [s]'); ylabel('RR Intervals [s]');
+
+    % Plot original intervals
+    plot(trr, rri ,'b-', 'LineWidth', 2);
+    legend_labels = {'RR intervals'};
+
+    % Plot filtered intervals
+    plot(tnn, nni, 'g-', 'LineWidth', 1);
+    legend_labels{end+1} = 'Filtered intervals';
+
+    if (~isempty(gqpost_outliers))
+        plot(trr(gqpost_outliers), rri(gqpost_outliers), 'm^', 'MarkerSize', markersize+1);
+        legend_labels{end+1} = 'gqpost outliers';
+    end
+
+    if (~isempty(poincare_outliers))
+        plot(trr(poincare_outliers), rri(poincare_outliers), 'rx', 'MarkerSize', markersize);
+        legend_labels{end+1} = 'poincare outliers';
+    end
+
+    if (~isempty(lp_outliers))
+        plot(trr(lp_outliers), rri(lp_outliers), 'ko', 'MarkerSize', markersize);
+        legend_labels{end+1} = 'lowpass outliers';
+    end
+
+    % Plot window average and thresholds
+    if (filter_lowpass)
+        plot(trr, rri_lp, 'k', trr, tresh_low, 'k--', trr, thresh_high, 'k--');
+        legend_labels = [legend_labels, {'window average', 'window threshold'}];
+    end
+
+    legend(legend_labels);
 end
 
 end
