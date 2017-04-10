b93ec1260fc20b65e4a69a6c5657db396b7a1884
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2017-04-10 15:23:58 +0300

    fig4: Use canine parameters file
    Also write NN intervals to txt file.

--- fig/results/f4_dogs_spectrum.m
+++ fig/results/f4_dogs_spectrum.m
@@ -4,8 +4,11 @@ output_dir = ['fig' filesep 'out'];
 folder = 'db/billman/';
 rec_types = {'pre-*-bsl'};
 
+% Load canine parameters
+rhrv_load_params('rhrv_params_canine');
+
 for rec_type_idx = 1:length(rec_types);
-    files = dir([folder sprintf('*-%s.dat', rec_types{rec_type_idx})])';
+    files = dir([folder sprintf('*%s.dat', rec_types{rec_type_idx})])';
     nfiles = length(files);
 
     for file_idx = 1:nfiles
@@ -15,15 +18,16 @@ for rec_type_idx = 1:length(rec_types);
         
         % Read data from record
         fprintf('-> Reading from %s\n', rec_name);
-        [nni, tnn, ~, ~] = ecgnn(rec_name, 'plot', false, 'filter_gqpost', false, ...
-                                 'gqconf', 'cfg/gqrs.billman.conf', ...
-                                 'filter_lowpass', true, 'filter_poincare', true);
+        [nni, tnn, ~, ~] = ecgnn(rec_name, 'plot', false);
        
         % Calculate spectrum
-        hrv_freq(nni, tnn, 'plot', true, 'window_minutes', [], 'band_factor', 120/75);
+        hrv_freq(nni, tnn, 'plot', true);
         
         % Print the figure
-        fig_print(gcf, [output_dir filesep 'freq_' name], 'title', rec_name);
+        fig_print(gcf, [output_dir filesep name '_freq'], 'title', rec_name);
+
+        % Write RR-intervals lengths
+        dlmwrite([output_dir filesep name '_nn.txt'], nni, 'delimiter', '\n', 'precision','%.3f');
     end
 
 end
