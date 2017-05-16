2b9f6db918b25e186bbc94930c55aaad8cfca648
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2017-05-16 10:48:53 +0300

    Sample Entropy: Support m=0
    Previously the code didn't produce a correct result for m=0 which is an
    edge case.

--- src/hrv/sample_entropy.m
+++ src/hrv/sample_entropy.m
@@ -7,9 +7,20 @@ function sampen = sample_entropy(sig, m, r)
 %   Physiological time-series analysis using approximate entropy and sample entropy.
 %   American Journal of Physiology. Heart and Circulatory Physiology, 278(6), H2039?H2049.
 
+N = length(sig);
+
+% Validations
+if (m < 0 || r < 0)
+    error('Invalid parameter values');
+end
+
 % Initialize template-match counters. A is the number of template matches
 % of length m+1, and B is the number of template matches of length m.
 A = 0; B = 0;
+if m == 0
+    % When m=0, B is defined as N*(N-1)/2.
+    B = N * (N-1) / 2;
+end
 
 % Create a matrix containing all templates (windows) of length m+1 (with m
 % samples overlap) that exist in the signal. Each row is a window.
@@ -18,32 +29,38 @@ num_templates = size(templates_mat, 1);
 
 % Loop over all templates, calcualting the Chebyshev distance between the
 % current template and all the following templates.
-for win_idx = 1:num_templates    
+for win_idx = 1:num_templates
     % Extract the current template and all the templates following it.
     curr_template = templates_mat(win_idx,:);
     next_templates_mat = templates_mat((win_idx+1):end,:);
-    
+
     % Calculate absolute difference vectors between the current template and the
     % each of the next templates.
     diff_mat = abs( bsxfun(@minus, curr_template, next_templates_mat) );
-    
+
     % Calculate Chebyshev distance: This is the max component of the absolute
-    % difference vector. Well calculate two distances: dist_A is the max
-    % difference component (Chebyshev distance) using all m+1 components,
-    % and dist_B is the Chebyshev distance using only the first m components.  
-    dist_B = max(diff_mat(:,1:end-1), [], 2); % max val of each row in diff_mat (without column m+1),
-    dist_A = max(dist_B, diff_mat(:,end));    % max between column m+1 and dist_B
-    
-    % A template match is a case where the Chebyshev distance between the
-    % current template and one of the next templates is less than r. Count
-    % the number of matches of length m+1 and of length m we have, and
-    % increment the appropriate counters.
-    A = A + sum(dist_A < r);
-    B = B + sum(dist_B < r);
+    % difference vector. We'll calculate two distances:
+    % dist_B: the Chebyshev distance using only the first m components.
+    dist_B = max(diff_mat(:,1:end-1), [], 2); % max val of each row in diff_mat, without column m+1.
+
+    % dist_A: the max difference component (Chebyshev distance) using all m+1 components
+    if m ~= 0
+        % max between column m+1 and dist_B (which is the max of columns 1..m)
+        dist_A = max(dist_B, diff_mat(:,end));
+
+        % A template match is a case where the Chebyshev distance between the
+        % current template and one of the next templates is less than r. Count
+        % the number of matches of length m+1 and of length m we have, and
+        % increment the appropriate counters.
+        A = A + sum(dist_A < r);
+        B = B + sum(dist_B < r);
+    else
+        % In case m is zero, dist_B is empty and dist_A is simply the diff_mat.
+        A = A + sum(diff_mat < r);
+    end
 end
 
 % Calculate the sample entropy value based on the number of template matches.
 sampen = -log(A / B);
-
 end
 
