af2384e81633aeb776962c7eb57ba07fc05cac18
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2017-06-28 15:28:12 +0300

    qrs_compare_set: Updates
    - Works with the refactored qrs_compare
    - Returns tables instead of structs
    - Fixed plot

--- src/ecg/qrs_compare_set.m
+++ src/ecg/qrs_compare_set.m
@@ -1,48 +1,45 @@
-function [ sqis ] = qrs_compare_set( set_dir, varargin )
-%QRS_COMPARE_SET Compares annotation QRS to gqrs on a set of wfdb records
+function [ sqis, stats ] = qrs_compare_set( set_dir, ann_ext, varargin )
+%QRS_COMPARE_SET Compares reference QRS detections to test detections on a set of wfdb records.
 %   Inputs:
-%       set_dir - directory path containing the wfdb files and annotations
+%       - set_dir: directory path containing the wfdb files and annotations
+%       - ann_ext: file extension of the annotation files.
+%       - varargin: Pass in name-value pairs to configure advanced options:
+%           - any parameter supported by qrs_compare() can be passed to this function.
+%           - plot: true/false whether to generate a plot. Defaults to true if no output arguments
+%                   were specified.
+%
+%   Outputs:
+%       - sqis: A table containing quality indices for each of the input files.
+%       - stats: A table containing the Mean and Gross values for the quality indices over all files.
 %
 
-%% === Input
-
-% Defaults
-DEFAULT_SHOULD_PLOT = false;
-DEFAULT_THRESH = 0.15; % 150 ms
-DEFAULT_ANN_EXT = 'atr';
-DEFAULT_QRS_DETECTOR = 'gqrs';
+%% Input
 
 % Define input
 p = inputParser;
-p.addRequired('set_dir', @(dir) exist(dir, 'dir'));
-p.addParameter('should_plot', DEFAULT_SHOULD_PLOT, @islogical);
-p.addParameter('bsqi_thresh', DEFAULT_THRESH, @isnumeric);
-p.addParameter('annotation_ext', DEFAULT_ANN_EXT, @isstr);
-p.addParameter('qrs_detector', DEFAULT_QRS_DETECTOR, @isstr);
+p.addRequired('set_dir', @(x) exist(x, 'dir'));
+p.addRequired('ann_ext', @(x) ischar(x) && ~isempty(x));
+p.addParameter('plot', nargout == 0, @islogical);
 
 % Get input
-p.parse(set_dir, varargin{:});
-should_plot = p.Results.should_plot;
-bsqi_thresh = p.Results.bsqi_thresh;
-annotation_ext = p.Results.annotation_ext;
-qrs_detector = p.Results.qrs_detector;
+p.KeepUnmatched = true;
+p.parse(set_dir, ann_ext, varargin{:});
+should_plot = p.Results.plot;
 
-%% === Process files
+%% Process files
 
 % Measure time
 t1 = tic;
 
 % Get files in dir that have an annotation with the desired ext
-files = dir([set_dir '*.' annotation_ext]);
-fprintf('** Found %d ''%s'' files in %s, processing...\n', length(files), annotation_ext, set_dir);
-
-% Pre-allocate struct array
-sqis = repmat(struct('recName','','Se',0,'PPV',0,'F1',0,'TP',0,'FP',0,'FN',0), length(files), 1);
+files = dir([set_dir filesep '*.' ann_ext]);
+fprintf('** Found %d ''%s'' files in %s, processing...\n', length(files), ann_ext, set_dir);
 
 % Process files
 N = length(files);
 N_error = 0;
-parfor i = 1:N
+sqis = table;
+for i = 1:N
     file = files(i);
     t2 = tic;
     
@@ -55,46 +52,64 @@ parfor i = 1:N
     % Make sure the record contains an ECG signal
     ecg_channel = get_signal_channel(recName);
     if (isempty(ecg_channel))
-        fprintf('** >> %10s: WARNING - No ECG data found. Skipping...\n', file.name);
+        fprintf('>> WARNING - No ECG data found in record %s. Skipping...\n', basename);
         N_error = N_error + 1;
         continue;
     end
     
     % Calculate SQI indices
-    sqis(i) = qrs_compare(recName, 'bsqi_thresh', bsqi_thresh, 'ecg_col', ecg_channel, 'qrs_detector', qrs_detector);
-    
+    params = p.Unmatched;
+    params.ann_ext = ann_ext;
+    sqi = qrs_compare(recName, params);
+
+    % Add file name to results
+    sqi.recName = basename;
+
+    % Add to output table
+    sqis = [sqis; struct2table(sqi)];
+
     % Print elapsed time
     elapsed_sec = toc(t2);
-    fprintf('** >> %10s: Se = %5.3f, PPV = %5.3f, F1 = %5.3f [elapsed = %6.3fs]\n', file.name, sqis(i).Se, sqis(i).PPV, sqis(i).F1, elapsed_sec);
+    fprintf('>> %s [elapsed = %6.3fs]\n', basename, elapsed_sec);
 end
 fprintf('** Done processing, total time: %.3fs\n', toc(t1));
 
-%% === Calculate average Se, PPV, F1
+% Remove the recName column and set row names instead
+sqis.Properties.RowNames = cellstr(sqis.recName);
+sqis.recName = [];
+
+%% Calculate mean & gross values for metrics
 N_no_error = N - N_error;
-mean_Se  = 100 * sum(cell2mat({sqis.Se})) / N_no_error;
-mean_PPV = 100 * sum(cell2mat({sqis.PPV}))/ N_no_error;
-mean_F1 =  100 * sum(cell2mat({sqis.F1})) / N_no_error;
+mean.SE  = 100 * sum(sqis.SE) / N_no_error;
+mean.PPV = 100 * sum(sqis.PPV)/ N_no_error;
+mean.F1 =  100 * sum(sqis.F1) / N_no_error;
 
-% Print results
-fprintf('** Mean:  Se=%5.1f%%, PPV=%5.1f%%, F1=%5.1f%%\n', mean_Se, mean_PPV, mean_F1);
+TP = sum(sqis.TP);
+FP = sum(sqis.FP);
+FN = sum(sqis.FN);
+gross.SE  = 100 * TP/(TP+FN);
+gross.PPV = 100 * TP/(FP+TP);
+gross.F1 =  2 * gross.SE * gross.PPV / (gross.SE + gross.PPV);
 
-%% === Calculate Gross Se, PPV, F1
-TP = sum(cell2mat({sqis.TP}));
-FP = sum(cell2mat({sqis.FP}));
-FN = sum(cell2mat({sqis.FN}));
-gross_Se  = 100 * TP/(TP+FN);
-gross_PPV = 100 * TP/(FP+TP);
-gross_F1 =  2 * gross_Se * gross_PPV / (gross_Se + gross_PPV);
+% Add to table
+stats = [
+    struct2table(mean, 'RowNames', {'Mean'});
+    struct2table(gross, 'RowNames', {'Gross'});
+];
 
-% Print results
-fprintf('** Gross: Se=%5.1f%%, PPV=%5.1f%%, F1=%5.1f%%\n', gross_Se, gross_PPV, gross_F1);
+% Print Results
+fprintf('\n** Results:\n'); disp(sqis);
+fprintf('\n** Summary:\n'); disp(stats);
 
-% Plots
-if ~should_plot; return; end;
-figure;
-bar([mean_Se, mean_PPV, mean_F1; gross_Se, gross_PPV, gross_F1] .* 100); grid on;
-legend('Se', 'PPV', 'F1');
-ylim([70, 100]);
-set(gca, 'XTickLabel', {'Mean', 'Gross'}, 'XTick', 1:2);
-set(gca, 'YTick', [70, 80, 90:1:100]);
-xlabel('Measures'); ylabel('% Value');
+%% Plot
+if should_plot
+    figure;
+    bar([mean.SE, mean.PPV, mean.F1; gross.SE, gross.PPV, gross.F1]);
+    grid on;
+
+    ylim([90, 100]);
+    set(gca, 'XTickLabel', {'Mean', 'Gross'}, 'XTick', 1:2);
+    set(gca, 'YTick', [90:1:98, 98.5:0.5:100]);
+    xlabel('Measures'); ylabel('% Value');
+    legend('SE', 'PPV', 'F1');
+end
