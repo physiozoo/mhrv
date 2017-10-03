526d6706e7c389bb7d142a8302ade54fb66650e7
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2017-10-03 13:15:54 +0300

    rdann wrapper: Support partial annotation loading
    Now supports from/to arguments to specify the range of samples to load
    annotations from.

--- src/wfdb/rdann.m
+++ src/wfdb/rdann.m
@@ -10,30 +10,41 @@ function [ ann ] = rdann( rec_name, ann_ext, varargin )
 %           - 'ann_types': A double-quoted string of PhysioNet annotation types that should be read,
 %                          e.g. '"N|"' to read both annotations of type 'N' and type '|'. Default is
 %                          empty, i.e. return annotations of any type.
+%           - from: Number of first sample to start detecting from (default 1)
+%           - to: Number of last sample to detect until (default [], i.e. end of signal)
 %   Output:
-%       - ann: A vector with the sample numbers containing the annotations.
+%       - ann: A vector with the sample numbers that have annotations.
 
 %% === Input
 
 % Defaults
 DEFAULT_ANN_TYPES = '';
+DEFAULT_FROM_SAMPLE = 1;
+DEFAULT_TO_SAMPLE = [];
 
 % Define input
 p = inputParser;
 p.addRequired('rec_name', @(x) isrecord(x, ann_ext));
 p.addRequired('ann_ext', @isstr);
 p.addParameter('ann_types', DEFAULT_ANN_TYPES, @ischar);
+p.addParameter('from', DEFAULT_FROM_SAMPLE, @(x) isnumeric(x) && isscalar(x));
+p.addParameter('to', DEFAULT_TO_SAMPLE, @(x) isnumeric(x) && (isscalar(x)||isempty(x)));
 
 % Get input
 p.parse(rec_name, ann_ext, varargin{:});
 ann_types = p.Results.ann_types;
+from_sample = p.Results.from;
+to_sample = p.Results.to;
 
 %% === Run rdann
 [rec_path, rec_filename, ~] = fileparts(rec_name);
 
 % Command to run rdann and cut only the annotation samples out
 rdann_path = get_wfdb_tool_path('rdann');
-command = sprintf('%s -e -r %s -a %s', rdann_path, rec_filename, ann_ext);
+command = sprintf('%s -e -r %s -a %s -f s%d', rdann_path, rec_filename, ann_ext, from_sample-1);
+if (~isempty(to_sample))
+    command = sprintf('%s -t s%d', command, to_sample-1);
+end
 
 % Add annotation types flag if necessary
 if (~isempty(ann_types))
