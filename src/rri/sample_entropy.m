c8fd3c5ae7b13fe46eb6bdfd565c7b74ccddeb68
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2018-06-01 14:58:35 +0300

    Sample Entropy: Performance improvement
    Improved implementation performance by roughly 40%.

--- src/rri/sample_entropy.m
+++ src/rri/sample_entropy.m
@@ -22,41 +22,52 @@ if m == 0
     B = N * (N-1) / 2;
 end
 
+% Convert to single for speed, since this algorithm requires alot of
+% in-memory copying
+sig = single(sig);
+
 % Create a matrix containing all templates (windows) of length m+1 (with m
 % samples overlap) that exist in the signal. Each row is a window.
 templates_mat = transpose( buffer(sig, m+1, m, 'nodelay') );
 num_templates = size(templates_mat, 1);
 
+next_templates_mat_m = templates_mat(:,1:m);
+next_templates_mat_1 = templates_mat(:,m+1);
+clear templates_mat;
+
 % Loop over all templates, calcualting the Chebyshev distance between the
 % current template and all the following templates.
 for win_idx = 1:num_templates
     % Extract the current template and all the templates following it.
-    curr_template = templates_mat(win_idx,:);
-    next_templates_mat = templates_mat((win_idx+1):end,:);
+    curr_template_m = next_templates_mat_m(1,:);
+    next_templates_mat_m = next_templates_mat_m(2:end,:);
+    curr_template_1 = next_templates_mat_1(1,:);
+    next_templates_mat_1 = next_templates_mat_1(2:end,:);
 
     % Calculate absolute difference vectors between the current template and the
     % each of the next templates.
-    diff_mat = abs( bsxfun(@minus, curr_template, next_templates_mat) );
+    diff_m = abs( next_templates_mat_m - curr_template_m );
+    diff_1 = abs( next_templates_mat_1 - curr_template_1 );
 
     % Calculate Chebyshev distance: This is the max component of the absolute
     % difference vector. We'll calculate two distances:
     % dist_B: the Chebyshev distance using only the first m components.
-    dist_B = max(diff_mat(:,1:end-1), [], 2); % max val of each row in diff_mat, without column m+1.
+    dist_B = max(diff_m, [], 2); % max val of each row in diff_m.
 
     % dist_A: the max difference component (Chebyshev distance) using all m+1 components
     if m ~= 0
         % max between column m+1 and dist_B (which is the max of columns 1..m)
-        dist_A = max(dist_B, diff_mat(:,end));
+        dist_A = max(dist_B, diff_1);
 
         % A template match is a case where the Chebyshev distance between the
         % current template and one of the next templates is less than r. Count
         % the number of matches of length m+1 and of length m we have, and
         % increment the appropriate counters.
-        A = A + sum(dist_A < r);
-        B = B + sum(dist_B < r);
+        A = A + nnz(dist_A < r);
+        B = B + nnz(dist_B < r);
     else
         % In case m is zero, dist_B is empty and dist_A is simply the diff_mat.
-        A = A + sum(diff_mat < r);
+        A = A + nnz(diff_1 < r);
     end
 end
 
